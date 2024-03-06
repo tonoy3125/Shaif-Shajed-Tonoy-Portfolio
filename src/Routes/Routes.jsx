1932774f1dcf47ab265913eeccf0e9fb5ddcb48a
tonoy3125 <shaifshajedt@gmail.com>
2024-03-06 23:43:40 +0600

    portfolio setup added by react

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import {
+    createBrowserRouter,
+
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div className="">Hello world!</div>,
+    },
+]);
+
+export default router
