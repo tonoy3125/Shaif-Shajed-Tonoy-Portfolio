2760eb88271dd3800b02c1e26b297c8b8e47eab6
tonoy3125 <shaifshajedt@gmail.com>
2024-03-23 17:04:43 +0600

    all device responsive banner added

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -4,25 +4,25 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 const Banner = () => {
     return (
         <div>
-            <div className="flex items-center px-5 flex-col md:flex-row mt-10 mb-10 semi-sm:gap-5 md:gap-7 lg:gap-0">
+            <div className="flex items-center px-5 flex-col md:flex-row mt-10 mb-10 xs:gap-5 md:gap-7 lg:gap-0">
                 {/* Heading */}
                 <div className="">
-                    <h1 className="text-[#FFFFFF59] md:text-3xl lg:text-5xl font-bold mb-5 font-cinzel">Hello, i’m</h1>
-                    <h1 className="text-[#c9f31d] font-bold md:text-3xl lg:text-5xl mb-4 font-cinzel" style={{ whiteSpace: "nowrap" }}>Shaif Shajed Tonoy</h1>
+                    <h1 className="text-[#FFFFFF59] sm:text-xl semi-sm:text-2xl md:text-3xl lg:text-5xl font-bold mb-5 font-cinzel">Hello, i’m</h1>
+                    <h1 className="text-[#c9f31d] font-bold sm:text-xl semi-sm:text-2xl md:text-3xl lg:text-5xl mb-4 font-cinzel" style={{ whiteSpace: "nowrap" }}>Shaif Shajed Tonoy</h1>
                     <h1 className="text-[#fff] md:text-xl lg:text-3xl font-bold mb-4 font-lora">Junior MERN-Stack-Developer</h1>
-                    <p className="text-white md:text-base md:w-[400px] lg:w-[500px] mb-10 font-lora">✨ Young Trailblazer in Full Stack Development 💻 | Mastering the art of code from UI/UX magic to server-side wizardry. Fluent in HTML, CSS, JavaScript, React,Firebase, Node.js, MongoDB. Committed to turning ideas into reality and making a mark in the digital realm. Let's build brilliance together! 🌐🚀</p>
+                    <p className="text-white xs:text-sm sm::text-base md:w-[400px] lg:w-[500px] mb-10 font-lora">✨ Young Trailblazer in Full Stack Development 💻 | Mastering the art of code from UI/UX magic to server-side wizardry. Fluent in HTML, CSS, JavaScript, React,Firebase, Node.js, MongoDB. Committed to turning ideas into reality and making a mark in the digital realm. Let's build brilliance together! 🌐🚀</p>
                     <div className="flex items-center gap-3">
-                        <button className="px-7 py-3 bg-[#c9f31d] flex items-center font-lora text-[#070707] hover:bg-[#070707] hover:text-white font-medium gap-1 rounded-lg"><span className="text-base">Hire Me</span> <span><MdKeyboardArrowRight className="text-base" /></span></button>
-                        <button className="px-7 py-3  flex items-center text-white  font-lora font-medium gap-1 rounded-lg"><span className="text-base">Download Resume
+                        <button className="xs:px-3 xs:py-1 sm:px-5 sm:py-2 semi-sm:px-7 semi-sm:py-3 bg-[#c9f31d] flex items-center font-lora text-[#070707] hover:bg-[#070707] hover:text-white font-medium gap-1 rounded-lg" style={{ whiteSpace: "nowrap" }}><span className="text-base">Hire Me</span> <span><MdKeyboardArrowRight className="text-base" /></span></button>
+                        <button className=" sm:px-5 sm:py-2 lg:px-7 lg:py-3  flex items-center text-white  font-lora font-medium gap-1 rounded-lg" style={{ whiteSpace: "nowrap" }}><span className="text-base">Download Resume
                         </span> <span><MdKeyboardArrowRight className="text-base" /></span></button>
                     </div>
                 </div>
                 {/* Image */}
                 <div className=" block md:hidden lg:block">
-                    <img className=" semi-sm:w-full md:w-3/4 semi-sm:mx-auto md:mx-0" src="https://i.ibb.co/fCnqG28/Black-Simple-Bold-Professional-Twitter-Profile-Picture-1.png" alt="" />
+                    <img className=" sm:w-full md:w-3/4 semi-sm:mx-auto md:mx-0" src="https://i.ibb.co/fCnqG28/Black-Simple-Bold-Professional-Twitter-Profile-Picture-1.png" alt="" />
                 </div>
                 {/* Stats */}
-                <div className=" p-10 semi-sm:w-96 md:w-72 bg-[#070707] rounded-xl">
+                <div className=" p-10 xs:w-72 sm:w-80 semi-sm:w-96 md:w-72 bg-[#070707] rounded-xl">
                     <div>
                         <h3 className="text-[40px] font-medium text-[#c9f31d] font-poppins mb-2"><CountUp start={0} end={13} duration={2} />+</h3>
 
