723d412bebab1d1002ab2e91ef9711eb7b2fba3c
tonoy3125 <shaifshajedt@gmail.com>
2024-03-25 01:30:58 +0600

    contact section heading and subheading margin added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
         <div className="max-w-7xl mx-auto mt-24 mb-20">
             <div className="flex items-start flex-col lg:flex-row gap-20 md:ml-5 lg:ml-0">
                 <div>
-                    <h5 className="text-base font-poppins font-medium text-[#b1b1b1]">Get In Touch</h5>
-                    <h1 className="font-poppins text-4xl lg:text-[45px] font-medium"><span className="text-[#fff]">Let’s Talk For your</span> <br className="lg:block md:hidden" /><span className="text-[#c9f31d]">Next Projects</span></h1>
+                    <h5 className="text-base font-poppins font-medium text-[#b1b1b1] mb-5">Get In Touch</h5>
+                    <h1 className="font-poppins text-4xl lg:text-[45px] font-medium mb-12"><span className="text-[#fff]">Let’s Talk For your</span> <br className="lg:block md:hidden" /><span className="text-[#c9f31d]">Next Projects</span></h1>
                     <p className="text-base text-[#b1b1b1] font-poppins lg:w-[400px]">Sed ut perspiciatis unde omnin natus totam rem aperiam eaque inventore veritatis</p>
                 </div>
                 <div>
@@ -17,7 +17,7 @@ const Contact = () => {
                         </div>
                         <div className="">
                             <h2 className=" text-base md:text-lg font-semibold text-white mb-2 lg:mb-4 font-poppins">Email Address <span className="text-[#c9f31d]">*</span></h2>
-                            <input className="pt-4 pb-4 pl-2 md:pl-8 md:py-5 md:w-[350px] lg:w-[380px] bg-[#292828]   text-base font-semibold font-lora text-white rounded border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" type="text" name="propertyname" placeholder="Enter Property Name Here" id="" />
+                            <input className="pt-4 pb-4 pl-2 md:pl-8 md:py-5 md:w-[350px] lg:w-[380px] bg-[#292828]   text-base font-semibold font-lora text-white rounded-xl border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" type="text" name="propertyname" placeholder="Enter Property Name Here" id="" />
                         </div>
                     </div>
                     <div className="flex items-center  flex-col md:flex-row gap-5 lg:gap-10" >
@@ -27,12 +27,12 @@ const Contact = () => {
                         </div>
                         <div className="">
                             <h2 className=" text-base md:text-lg font-semibold text-white mb-2 lg:mb-4 font-poppins">Subject <span className="text-[#c9f31d]">*</span></h2>
-                            <input className="pt-4 pb-4 pl-2 md:pl-8 md:py-5 md:w-[350px] lg:w-[380px] bg-[#292828]   text-base font-semibold font-lora text-white rounded border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" type="text" name="propertyname" placeholder="Enter Property Name Here" id="" />
+                            <input className="pt-4 pb-4 pl-2 md:pl-8 md:py-5 md:w-[350px] lg:w-[380px] bg-[#292828]   text-base font-semibold font-lora text-white rounded-xl border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" type="text" name="propertyname" placeholder="Enter Property Name Here" id="" />
                         </div>
                     </div>
                     <div className="mb-10 mt-10 ">
                         <h2 className=" text-base md:text-lg font-semibold text-white mb-2 lg:mb-4 font-poppins">Textarea <span className="text-[#c9f31d]">*</span></h2>
-                        <textarea id="message" rows="4" className="block pt-4 pb-4 pl-2 md:pl-8 md:py-5 w-full text-base font-semibold font-lora text-white bg-[#292828] rounded-lg   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" placeholder="Write message"></textarea>
+                        <textarea id="message" rows="4" className="block pt-4 pb-4 pl-2 md:pl-8 md:py-5 w-full text-base font-semibold font-lora text-white bg-[#292828] rounded-xl   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border  border-[#353535] focus:border-[#c9f31d] focus:outline-none" placeholder="Write message"></textarea>
 
                     </div>
                 </div>
