4da89f7f2a49bc627b5ee96bfabebd3a73df8159
tonoy3125 <shaifshajedt@gmail.com>
2024-03-27 17:04:29 +0600

    text added on the experience section

--- src/Pages/Experience/Experience.jsx
+++ src/Pages/Experience/Experience.jsx
@@ -7,27 +7,27 @@ const Experience = () => {
                 <div className="grid grid-cols-1  gap-5 md:px-14 px-4 lg:px-0">
 
                     <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
-                        <h1 className="text-xl font-semibold text-white">React.js Project Developer</h1>
-                        <h3 className="text-base font-medium text-white flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Dynamic UI Creation:</h3>
-                        <p className=" font-medium text-white text-sm">Crafted captivating user interfaces & web interaction.</p>
+                        <h1 className="text-xl font-semibold text-[#fff]">React.js Project Developer</h1>
+                        <h3 className="text-base font-medium text-[#fff] flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Dynamic UI Creation:</h3>
+                        <p className=" font-medium text-[#fff] text-sm">Crafted captivating user interfaces & web interaction.</p>
                         <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Tailwind CSS Mastery: </h3>
                         <p className="text-sm font-medium text-white">Leveraged Tailwind CSS for refined, modernized web aesthetics.</p>
 
                     </div>
                     <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
-                        <h1 className="text-xl font-semibold text-white">Database Integration With MongoDB</h1>
-                        <h3 className="text-base font-medium text-white flex items-center mt-2">  <IoMdArrowDropright></IoMdArrowDropright> Efficient Data Storage:</h3>
-                        <p className="text-sm font-medium text-white">Utilized MongoDB for efficient, scalable data storage..</p>
-                        <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Enhanced Backend Functionality: </h3>
-                        <p className="text-sm font-medium text-white">Integrated MongoDB, optimized backend for performance.</p>
+                        <h1 className="text-xl font-semibold text-[#fff]">Database Integration With MongoDB</h1>
+                        <h3 className="text-base font-medium text-[#fff] flex items-center mt-2">  <IoMdArrowDropright></IoMdArrowDropright> Efficient Data Storage:</h3>
+                        <p className="text-sm font-medium text-[#fff]">Utilized MongoDB for efficient, scalable data storage..</p>
+                        <h3 className="text-base font-medium text-[#fff] flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Enhanced Backend Functionality: </h3>
+                        <p className="text-sm font-medium text-[#fff]">Integrated MongoDB, optimized backend for performance.</p>
                     </div>
                     <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
-                        <h1 className="text-xl font-semibold text-white">Version Control Skills</h1>
-                        <h3 className="text-base font-medium text-white flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Git Proficiency:</h3>
-                        <p className="text-sm font-medium text-white">
+                        <h1 className="text-xl font-semibold text-[#fff]">Version Control Skills</h1>
+                        <h3 className="text-base font-medium text-[#fff] flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Git Proficiency:</h3>
+                        <p className="text-sm font-medium text-[#fff]">
                             Expert in Git for collaborative development..</p>
-                        <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Repository Management:</h3>
-                        <p className="text-sm font-medium text-white">Managed version-controlled repositories for code integrity.</p>
+                        <h3 className="text-base font-medium text-[#fff] flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Repository Management:</h3>
+                        <p className="text-sm font-medium text-[#fff]">Managed version-controlled repositories for code integrity.</p>
                     </div>
                 </div>
             </div>
