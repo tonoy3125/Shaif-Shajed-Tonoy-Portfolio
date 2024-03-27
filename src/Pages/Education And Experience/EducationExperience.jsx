ccb8bc80e37a02f4090f28e7b9b497008d17dad2
tonoy3125 <shaifshajedt@gmail.com>
2024-03-27 16:50:52 +0600

    experience design added

--- src/Pages/Education And Experience/EducationExperience.jsx
+++ src/Pages/Education And Experience/EducationExperience.jsx
@@ -7,19 +7,24 @@ import { SlBadge } from "react-icons/sl";
 const EducationExperience = () => {
 
     return (
-        <div className=" max-w-6xl mx-auto">
-
+        <div className=" max-w-6xl mx-auto pt-10 pb-10">
 
             <div className="flex lg:flex-row flex-col justify-between gap-6">
                 <div>
-                    <div className="flex items-center gap-5 mb-10 text-white">
+                    <div className="flex items-center gap-5 mb-10 text-[#fff]">
                         <SlBadge className="text-5xl" />
                         <h1 className="text-5xl font-bold">My Experience</h1>
                     </div>
                     <Education></Education>
                 </div>
-
                 
+                <div>
+                    <div className="flex items-center gap-5 mb-10 text-[#fff]">
+                        <FaUserGraduate className="text-5xl" />
+                        <h1 className="text-5xl font-bold">My Education</h1>
+                    </div>
+                    <Experience></Experience>
+                </div>
 
             </div>
         </div>
---
+++ src/Pages/Experience/Experience.jsx
@@ -0,0 +1,38 @@
+import { IoMdArrowDropright } from "react-icons/io";
+
+const Experience = () => {
+    return (
+        <div>
+            <div className="max-w-5xl mx-auto mb-20">
+                <div className="grid grid-cols-1  gap-5 md:px-14 px-4 lg:px-0">
+
+                    <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
+                        <h1 className="text-xl font-semibold text-white">React.js Project Developer</h1>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Dynamic UI Creation:</h3>
+                        <p className=" font-medium text-white text-sm">Crafted captivating user interfaces & web interaction.</p>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Tailwind CSS Mastery: </h3>
+                        <p className="text-sm font-medium text-white">Leveraged Tailwind CSS for refined, modernized web aesthetics.</p>
+
+                    </div>
+                    <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
+                        <h1 className="text-xl font-semibold text-white">Database Integration With MongoDB</h1>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2">  <IoMdArrowDropright></IoMdArrowDropright> Efficient Data Storage:</h3>
+                        <p className="text-sm font-medium text-white">Utilized MongoDB for efficient, scalable data storage..</p>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Enhanced Backend Functionality: </h3>
+                        <p className="text-sm font-medium text-white">Integrated MongoDB, optimized backend for performance.</p>
+                    </div>
+                    <div className="border border-[#353535] shadow-2xl  bg-[#292828]  rounded py-5 px-2">
+                        <h1 className="text-xl font-semibold text-white">Version Control Skills</h1>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2"> <IoMdArrowDropright></IoMdArrowDropright>Git Proficiency:</h3>
+                        <p className="text-sm font-medium text-white">
+                            Expert in Git for collaborative development..</p>
+                        <h3 className="text-base font-medium text-white flex items-center mt-2"><IoMdArrowDropright></IoMdArrowDropright> Repository Management:</h3>
+                        <p className="text-sm font-medium text-white">Managed version-controlled repositories for code integrity.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
