904501b8613a270f17293c3501a6e056c0d52aa0
tonoy3125 <shaifshajedt@gmail.com>
2024-04-02 01:48:40 +0600

    modal responsive for 425 px mobile device

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -62,15 +62,15 @@ const Projects = () => {
                     <div className="modal-content">
                         <span className={`modal-close ${modalOpen ? 'rotate' : ''}`} onClick={closeModal}><ImCross /></span>
                         {/* Modal content */}
-                        <img className=' md:px-7 lg:px-[40px]' src="https://i.ibb.co/gd2dpPv/Screenshot-2024-03-28-at-13-57-06-BD-Quick-School.png" alt="" />
-                        <div className='bg-white md:px-7 lg:px-[40px]'>
-                            <div className='flex items-center gap-7 lg:gap-20'>
+                        <img className=' semi-sm:px-5 md:px-7 lg:px-[40px]' src="https://i.ibb.co/gd2dpPv/Screenshot-2024-03-28-at-13-57-06-BD-Quick-School.png" alt="" />
+                        <div className='bg-white semi-sm:px-5 md:px-7 lg:px-[40px]'>
+                            <div className='flex flex-col md:flex-row items-center gap-7 lg:gap-20'>
                                 <div className='lg:flex-1 mt-10'>
-                                    <h1 className='md:text-2xl lg:text-4xl font-poppins font-bold mb-5' style={{ whiteSpace: "nowrap" }}>Bd Quick School</h1>
-                                    <p className='font-lora md:text-base text-[#0f0715] font-medium'>They are was greater open above shelter lets itself under appear sixth open gathering made upon can't own above midst gathering gathered he one us saying can't divide.</p>
+                                    <h1 className='semi-sm:text-3xl md:text-2xl lg:text-4xl font-poppins font-bold mb-5' style={{ whiteSpace: "nowrap" }}>Bd Quick School</h1>
+                                    <p className='font-lora text-base text-[#0f0715] font-medium'>They are was greater open above shelter lets itself under appear sixth open gathering made upon can't own above midst gathering gathered he one us saying can't divide.</p>
                                 </div>
                                 <div className='lg:flex-1'>
-                                    <div className='flex items-center gap-5 lg:gap-40 mt-10'>
+                                    <div className='flex flex-col semi-sm:flex-row  md:items-center gap-5 lg:gap-40 mt-10'>
                                         <div style={{ whiteSpace: "nowrap" }}>
                                             <h3 className='text-base font-poppins'>Category</h3>
                                             <p className='text-base font-lora font-medium'>Full Stack  Project</p>
@@ -80,7 +80,7 @@ const Projects = () => {
                                             <p className='text-base font-lora font-medium'>Education Platform</p>
                                         </div>
                                     </div>
-                                    <div className='flex items-center gap-7 lg:gap-[167px] mt-10'>
+                                    <div className='flex flex-col semi-sm:flex-row md:items-center gap-7 lg:gap-[167px] mt-10'>
                                         <div>
                                             <h3 className='text-base font-poppins'>Start Date</h3>
                                             <p className='text-base font-lora font-medium'>January 20, 2024</p>
@@ -92,10 +92,10 @@ const Projects = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className='flex items-center md:gap-2 lg:gap-2 mt-10' style={{ whiteSpace: "nowrap" }}>
+                            <div className='flex flex-col md:flex-row md:items-center semi-sm:gap-5 md:gap-2 lg:gap-2 mt-10' style={{ whiteSpace: "nowrap" }}>
                                 <a href="https://quick-school-client.netlify.app/">
                                     <div className='buttonContainer'>
-                                        <button className='flex items-center lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
                                             <span className='text-sm lg:text-base font-lora font-semibold'>Live Preview</span>
                                             <MdOutlineArrowOutward className='text-xl arrowIcon' />
                                         </button>
@@ -103,7 +103,7 @@ const Projects = () => {
                                 </a>
                                 <a href="https://github.com/Shiham123/bd-quick-school-client" style={{ whiteSpace: "nowrap" }}>
                                     <div className='buttonContainer'>
-                                        <button className='flex items-center lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
                                             <span className='lg:text-base font-lora font-semibold'>Client-Side-Repo</span>
                                             <MdOutlineArrowOutward className='text-xl arrowIcon' />
                                         </button>
@@ -111,7 +111,7 @@ const Projects = () => {
                                 </a>
                                 <a href="https://github.com/Shiham123/bd-quick-school-server" style={{ whiteSpace: "nowrap" }}>
                                     <div className='buttonContainer'>
-                                        <button className='flex items-center lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
                                             <span className='lg:text-base font-lora font-semibold'>Server-Side-Repo</span>
                                             <MdOutlineArrowOutward className='text-xl arrowIcon' />
                                         </button>
