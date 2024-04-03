c229d22ef641f759eaf2164825f66bbb46b1518d
tonoy3125 <shaifshajedt@gmail.com>
2024-04-04 04:44:35 +0600

    technology added project three

--- src/Components/Project Three/Description/Description.jsx
+++ src/Components/Project Three/Description/Description.jsx
@@ -5,38 +5,28 @@ const Description = () => {
         <div>
             <h1 className="text-4xl font-bold font-poppins mt-5 mb-5">Technology</h1>
             <div className="flex flex-wrap items-center font-lora font-medium">
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 md:mb-0">
+                <div className="w-full md:w-1/4 lg:w-1/4 mb-4 md:mb-0">
                     <ul className="list-disc ml-4">
                         <li>Tailwind</li>
                         <li>React</li>
                     </ul>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 md:mb-0">
-                    <ul className="list-disc ml-4">
-                        <li>Sslcommerz</li>
-                        <li>Tanstack-query</li>
-                    </ul>
-                </div>
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 md:mb-0">
-                    <ul className="list-disc ml-4">
-                        <li>Redux</li>
-                        <li>Framer-Motion</li>
-                    </ul>
-                </div>
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 md:mb-0">
+
+
+                <div className="w-full md:w-1/4 lg:w-1/4 mb-4 md:mb-0">
                     <ul className="list-disc ml-4">
 
-                        <li>Material-ui</li>
-                        <li>Zigo Cloud</li>
+                        <li>React Tab</li>
+                        <li>Daisy Ui</li>
                     </ul>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 md:mb-0">
+                <div className="w-full md:w-1/4 lg:w-1/4 mb-4 md:mb-0">
                     <ul className="list-disc ml-4">
                         <li>JWT</li>
                         <li>Firebase</li>
                     </ul>
                 </div>
-                <div className="w-full md:w-1/3 lg:w-1/6 mb-4 lg:mb-0">
+                <div className="w-full md:w-1/4 lg:w-1/4  lg:mb-0">
                     <ul className="list-disc ml-4">
                         <li>Express js</li>
                         <li>MongoDB</li>
@@ -44,7 +34,7 @@ const Description = () => {
                 </div>
             </div>
             <h1 className="text-4xl font-bold font-poppins mt-5 mb-5">Project Description</h1>
-            <p className="text-base font-lora font-medium ">At BD Quick School, we believe in the power of knowledge to make a difference in people's lives. Our platform is crafted to offer a seamless and enjoyable learning experience for individuals from all walks of life, regardless of age or background. Whether you're a student looking to excel in your studies, a professional aiming to enhance your skills, or simply someone with a passion for lifelong learning, we cater to everyone's needs. With our user-friendly interface and engaging content, discovering new concepts and expanding your knowledge has never been easier. Join us on our journey to unlock the endless possibilities that education has to offer. Together, let's embark on a fulfilling learning experience that will empower you to achieve your goals and thrive in today's ever-changing world.</p>
+            <p className="text-base font-lora font-medium ">Job Recruiter is an online platform designed for job seekers and employers alike. It offers a simple and efficient way to find jobs and post job listings. Job seekers can browse through various job opportunities and submit bids for the ones that match their skills and interests. Meanwhile, employers can easily add job listings and connect with potential candidates. The platform streamlines the job search process, making it accessible and straightforward for everyone involved. Whether you're looking for your next career move or seeking to fill a vacant position, CareerHub Connect provides a user-friendly solution for all your job-related needs.</p>
             <h1 className="text-4xl font-bold font-poppins mt-5 mb-5">Unique Features</h1>
             <ul className="font-lora">
                 <ul className="list-disc pl-5">
