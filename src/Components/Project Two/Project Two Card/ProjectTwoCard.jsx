cd3fd3cf3a29244ed4d9b4008775bcc7de6fa478
tonoy3125 <shaifshajedt@gmail.com>
2024-04-03 15:00:08 +0600

    project two modal added

--- src/Components/Project Two/Project Two Card/ProjectTwoCard.jsx
+++ src/Components/Project Two/Project Two Card/ProjectTwoCard.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
+import './ProjectTwoCard.css'
+import { ImCross } from "react-icons/im";
 
 
 const ProjectTwoCard = () => {
@@ -46,10 +48,79 @@ const ProjectTwoCard = () => {
                     <MdOutlineArrowOutward className={`xs:text-2xl semi-sm:text-3xl lg:text-4xl rotate-icon ${isHovered ? 'rotate-full' : ''}`} />
 
                 </div>
-
-
-
             </div>
+            {modalOpen &&
+                <div className="modal-overlay" onClick={handleModalClick}>
+                    <div className="modal-content">
+                        <span className={`modal-close ${modalOpen ? 'rotate' : ''}`} onClick={closeModal}><ImCross /></span>
+                        {/* Modal content */}
+                        <img className='sm:px-4 xs:px-3 semi-sm:px-5 md:px-7 lg:px-[35px]' src="https://i.ibb.co/TBNmyjb/Screenshot-2024-04-03-at-13-55-33-Evergreen-Estates-Home.png" alt="" />
+                        <div className='bg-white xs:px-3 sm:px-4 semi-sm:px-5 md:px-7 lg:px-[35px]'>
+                            <div className='flex flex-col md:flex-row items-center gap-7 lg:gap-20'>
+                                <div className='lg:flex-1 mt-10'>
+                                    <h1 className='xs:text-xl sm:text-2xl semi-sm:text-3xl md:text-2xl lg:text-4xl font-poppins font-bold mb-5' style={{ whiteSpace: "nowrap" }}>Bd Quick School</h1>
+                                    <p className='font-lora text-base text-[#0f0715] font-medium'>They are was greater open above shelter lets itself under appear sixth open gathering made upon can't own above midst gathering gathered he one us saying can't divide.</p>
+                                </div>
+                                <div className='lg:flex-1'>
+                                    <div className='flex flex-col semi-sm:flex-row pr-[73px] sm:pr-[115px] semi-sm:pr-0 semi-sm:items-center gap-5 lg:gap-40 mt-10'>
+                                        <div style={{ whiteSpace: "nowrap" }}>
+                                            <h3 className='text-base font-poppins'>Category</h3>
+                                            <p className='text-base font-lora font-medium'>Full Stack  Project</p>
+                                        </div>
+                                        <div style={{ whiteSpace: "nowrap" }}>
+                                            <h3 className='text-base font-poppins'>Type</h3>
+                                            <p className='text-base font-lora font-medium'>Education Platform</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex flex-col semi-sm:flex-row md:items-center gap-7 lg:gap-[167px] mt-10'>
+                                        <div>
+                                            <h3 className='text-base font-poppins'>Start Date</h3>
+                                            <p className='text-base font-lora font-medium'>January 20, 2024</p>
+                                        </div>
+                                        <div>
+                                            <h3 className='text-base font-poppins'>End Date</h3>
+                                            <p className='text-base font-lora font-medium'>March 10, 2024</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col md:flex-row md:items-center gap-5 md:gap-2 lg:gap-2 mt-10' style={{ whiteSpace: "nowrap" }}>
+                                <a href="https://quick-school-client.netlify.app/">
+                                    <div className='buttonContainer'>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                            <span className='text-sm lg:text-base font-lora font-semibold'>Live Preview</span>
+                                            <MdOutlineArrowOutward className='text-xl arrowIcon' />
+                                        </button>
+                                    </div>
+                                </a>
+                                <a href="https://github.com/Shiham123/bd-quick-school-client" style={{ whiteSpace: "nowrap" }}>
+                                    <div className='buttonContainer'>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                            <span className='lg:text-base font-lora font-semibold'>Client-Side-Repo</span>
+                                            <MdOutlineArrowOutward className='text-xl arrowIcon' />
+                                        </button>
+                                    </div>
+                                </a>
+                                <a href="https://github.com/Shiham123/bd-quick-school-server" style={{ whiteSpace: "nowrap" }}>
+                                    <div className='buttonContainer'>
+                                        <button className='flex items-center gap-5 md:gap-0 lg:gap-2 text-white buttoninner px-7 py-3 rounded-full '>
+                                            <span className='lg:text-base font-lora font-semibold'>Server-Side-Repo</span>
+                                            <MdOutlineArrowOutward className='text-xl arrowIcon' />
+                                        </button>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className='mt-14 pb-7'>
+                                {/* <Slider /> */}
+                            </div>
+                            <div className='pb-10'>
+                                {/* <Description /> */}
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            }
         </div>
     );
 };
