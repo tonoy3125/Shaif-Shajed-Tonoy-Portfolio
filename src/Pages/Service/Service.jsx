3b768b52c13d44f88e5d507142ee5ddf536ce5be
tonoy3125 <shaifshajedt@gmail.com>
2024-04-04 21:40:59 +0600

    service card hover border color and service css added

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -1,10 +1,11 @@
 import { FiArrowUpRight } from "react-icons/fi";
+import './Service.css'
 
 const Service = () => {
     return (
         <div className="max-w-7xl mx-auto">
             <div className="flex items-center">
-                <div className="flex items-center gap-28 border rounded-[15px] px-10 py-10">
+                <div className="flex items-center gap-28  px-10 py-10 service-card">
                     <div className="flex items-start gap-16">
                         <p className="text-2xl font-semibold font-poppins text-white">01</p>
                         <div>
@@ -12,8 +13,8 @@ const Service = () => {
                             <p className="text-[#FFFFFFA6] text-base font-lora font-medium">Tailored, efficient, user-centric web solutions..</p>
                         </div>
                     </div>
-                    <div className="px-2 py-2 border rounded-full">
-                        <FiArrowUpRight className="text-white text-2xl" />
+                    <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
+                        <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
                     </div>
                 </div>
             </div>
