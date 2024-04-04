4a897c2c6d4313ccfe86960439e6d1de0918f5e3
tonoy3125 <shaifshajedt@gmail.com>
2024-04-05 00:10:05 +0600

    service card all card design added

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -4,8 +4,8 @@ import './Service.css'
 const Service = () => {
     return (
         <div className="max-w-7xl mx-auto">
-            <div className="flex items-center gap-5">
-                <div className="flex items-center gap-28  px-5 py-10 service-card">
+            <div className="flex items-center gap-5 mb-5">
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
                     <div className="flex items-start gap-10">
                         <p className="text-2xl font-semibold font-poppins text-white">01</p>
                         <div>
@@ -17,12 +17,64 @@ const Service = () => {
                         <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
                     </div>
                 </div>
-                <div className="flex items-center gap-28  px-5 py-10 service-card">
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
                     <div className="flex items-start gap-16">
                         <p className="text-2xl font-semibold font-poppins text-white">02</p>
                         <div>
                             <h1 className="text-white text-2xl font-lora font-semibold mb-2" >Responsive Web Design</h1>
-                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Adaptive user friendly cross-device web layouts</p>
+                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Adaptive user friendly and device web layouts..</p>
+                        </div>
+                    </div>
+                    <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
+                        <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center gap-5 mb-5">
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
+                    <div className="flex items-start gap-16">
+                        <p className="text-2xl font-semibold font-poppins text-white">03</p>
+                        <div>
+                            <h1 className="text-white text-2xl font-lora font-semibold mb-2" >API Dev & Integration</h1>
+                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Efficient, compatible and smooth data sharing</p>
+                        </div>
+                    </div>
+                    <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
+                        <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
+                    </div>
+                </div>
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
+                    <div className="flex items-start gap-16">
+                        <p className="text-2xl font-semibold font-poppins text-white">04</p>
+                        <div>
+                            <h1 className="text-white text-2xl font-lora font-semibold mb-2" >Progressive Web Apps</h1>
+                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Offline-enabled responsive native experiences</p>
+                        </div>
+                    </div>
+                    <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
+                        <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center gap-5">
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
+                    <div className="flex items-start gap-16">
+                        <p className="text-2xl font-semibold font-poppins text-white">05</p>
+                        <div>
+                            <h1 className="text-white text-2xl font-lora font-semibold mb-2" >Real-time Web Application</h1>
+                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Instant, interactive & user free experiences.....</p>
+                        </div>
+                    </div>
+                    <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
+                        <FiArrowUpRight className="text-white hover:text-[#131313] text-2xl icon" />
+                    </div>
+                </div>
+                <div className="flex items-center gap-28 w-[630px] px-5 py-10 service-card">
+                    <div className="flex items-start gap-16">
+                        <p className="text-2xl font-semibold font-poppins text-white">06</p>
+                        <div>
+                            <h1 className="text-white text-2xl font-lora font-semibold mb-2" >Single Page Applications</h1>
+                            <p className="text-[#FFFFFFA6] text-base font-lora font-medium" style={{ whiteSpace: "nowrap" }}>Flexible, intuitive, customizable data platform.</p>
                         </div>
                     </div>
                     <div className="px-2 py-2 bg-[#131313] border border-[#353535] rounded-full icon-container">
