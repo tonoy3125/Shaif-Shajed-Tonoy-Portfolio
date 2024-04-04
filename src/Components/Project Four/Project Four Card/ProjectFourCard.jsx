aa7d19faa2f376138549d80a0a8634a8ea9fe00d
tonoy3125 <shaifshajedt@gmail.com>
2024-04-04 15:58:26 +0600

    some chnages commit

--- src/Components/Project Four/Project Four Card/ProjectFourCard.jsx
+++ src/Components/Project Four/Project Four Card/ProjectFourCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect, useState } from "react";
 import { ImCross } from "react-icons/im";
 import { MdOutlineArrowOutward } from "react-icons/md";
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import './Projects.css'
 const Projects = () => {
 
     return (
-        <div className="pt-10 pb-10">
+        <div className="pt-20 pb-20">
             <div className="max-w-6xl mx-auto">
                 <h5 className="text-base font-medium text-white/50 text-center mb-5 font-poppins">Latest Works</h5>
                 <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium text-center font-poppins"><span className="text-[#fff] mb-10">Explore My Popular</span> <span className="text-[#c9f31d]">Projects</span></h1>
