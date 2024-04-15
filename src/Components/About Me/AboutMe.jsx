cfa8375fb97ec389b02f299d5532799f7f70ea1d
tonoy3125 <shaifshajedt@gmail.com>
2024-04-15 22:56:08 +0600

    responsive issue solved

--- src/Components/About Me/AboutMe.jsx
+++ src/Components/About Me/AboutMe.jsx
@@ -8,12 +8,12 @@ const AboutMe = () => {
         <div className="bg-[#070707] pt-28 pb-28">
             <div className="max-w-7xl mx-auto">
                 <div className="flex flex-col lg:flex-row gap-20">
-                    <div className="sm:mx-3 semi-sm:mx-5 md:mx-10 lg:mx-0">
+                    <div className="xs:mx-3 sm:mx-5 semi-sm:mx-5 md:mx-10 lg:mx-0">
                         <h5 className="text-base font-medium text-white/50  mb-5 font-poppins">About Me</h5>
                         <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium   font-poppins"><span className="text-[#fff]">Professional</span> <span className="text-[#c9f31d]">Problem Solutions </span>
                             <span className="text-[#fff]">For Digital Products</span></h1>
                         <p className="text-base text-[#b1b1b1] font-poppins mt-7">At vero eos et accusamus etodio dignissimos ducimus praesentium voluptatum corrupti quos dolores quas molestias excepturi sint occaecati cupiditate provident qui officia deserunt mollitia animi, id est laborum et dolorum</p>
-                        <div className="flex flex-col md:flex-row md:items-center sm:gap-3 semi-sm:gap-5 md:gap-28 lg:gap-40 sm:mt-3 semi-sm:mt-5 md:mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center gap-3 semi-sm:gap-5 md:gap-28 lg:gap-40 mt-5 semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Branding & Design</span>
@@ -23,7 +23,7 @@ const AboutMe = () => {
                                 <span className="text-white text-xl font-lora font-semibold">Digital Marketing</span>
                             </div>
                         </div>
-                        <div className="flex flex-col md:flex-row md:items-center sm:gap-3 semi-sm:gap-5 md:gap-[115px] lg:gap-[164px] sm:mt-3 semi-sm:mt-5 md:mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center gap-3 semi-sm:gap-5 md:gap-[115px] lg:gap-[164px] mt-3 semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Web Development</span>
@@ -36,7 +36,7 @@ const AboutMe = () => {
                         <div className=" mt-10 py-5 px-5 md:px-8 md:max-w-[630px] rounded-[20px] bg-[#292828] border border-[#353535]">
                             <div className="flex flex-col md:flex-row items-start md:items-center gap-16">
                                 <div className="flex items-center gap-4">
-                                    <div className=" px-3 py-3 rounded-full bg-[#c9f31d] flex-1">
+                                    <div className=" px-2 py-2 semi-sm:px-3 semi-sm:py-3 rounded-full bg-[#c9f31d] flex-1">
                                         <MdOutlineEmail className="text-2xl" />
                                     </div>
                                     <div className="flex-1">
@@ -45,7 +45,7 @@ const AboutMe = () => {
                                     </div>
                                 </div>
                                 <div className="flex items-center gap-4">
-                                    <div className=" px-3 py-3 rounded-full bg-[#c9f31d]">
+                                    <div className=" px-2 py-2 semi-sm:px-3 semi-sm:py-3 rounded-full bg-[#c9f31d]">
                                         <FaPhone className="text-2xl" />
                                     </div>
                                     <div>
@@ -57,7 +57,7 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div>
-                        <img className=" sm:max-w-[350px] semi-sm:max-w-[380px]  md:max-w-[450px] mx-auto md:h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
+                        <img className=" xs:max-w-[295px] sm:max-w-[340px] semi-sm:max-w-[380px]  md:max-w-[450px] mx-auto md:h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
                     </div>
                 </div>
             </div>
