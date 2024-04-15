fbcb361074e4f268802ba765b9993527e405710a
tonoy3125 <shaifshajedt@gmail.com>
2024-04-15 22:11:04 +0600

    some chnages about me section for tablet responsive

--- src/Components/About Me/AboutMe.jsx
+++ src/Components/About Me/AboutMe.jsx
@@ -5,15 +5,15 @@ import { FaPhone } from "react-icons/fa";
 
 const AboutMe = () => {
     return (
-        <div className="bg-[#070707] pt-28 pb-28">   
+        <div className="bg-[#070707] pt-28 pb-28">
             <div className="max-w-7xl mx-auto">
-                <div className="flex gap-20">
-                    <div>
+                <div className="flex flex-col lg:flex-row gap-20">
+                    <div className="semi-sm:mx-5 md:mx-10 lg:mx-0">
                         <h5 className="text-base font-medium text-white/50  mb-5 font-poppins">About Me</h5>
                         <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium   font-poppins"><span className="text-[#fff]">Professional</span> <span className="text-[#c9f31d]">Problem Solutions</span>
                             <span className="text-[#fff]">For Digital Products</span></h1>
                         <p className="text-base text-[#b1b1b1] font-poppins mt-7">At vero eos et accusamus etodio dignissimos ducimus praesentium voluptatum corrupti quos dolores quas molestias excepturi sint occaecati cupiditate provident qui officia deserunt mollitia animi, id est laborum et dolorum</p>
-                        <div className="flex items-center gap-40 mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center semi-sm:gap-5 md:gap-28 lg:gap-40 semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Branding & Design</span>
@@ -23,7 +23,7 @@ const AboutMe = () => {
                                 <span className="text-white text-xl font-lora font-semibold">Digital Marketing</span>
                             </div>
                         </div>
-                        <div className="flex items-center gap-[164px] mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center semi-sm:gap-5 md:gap-[115px] lg:gap-[164px] semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Web Development</span>
@@ -33,8 +33,8 @@ const AboutMe = () => {
                                 <span className="text-white text-xl font-lora font-semibold">Product Design</span>
                             </div>
                         </div>
-                        <div className=" mt-10 py-5 px-8 max-w-[630px] rounded-[20px] bg-[#292828] border border-[#353535]">
-                            <div className="flex items-center gap-16">
+                        <div className=" mt-10 py-5 px-5 md:px-8 md:max-w-[630px] rounded-[20px] bg-[#292828] border border-[#353535]">
+                            <div className="flex flex-col md:flex-row items-start md:items-center gap-16">
                                 <div className="flex items-center gap-4">
                                     <div className=" px-3 py-3 rounded-full bg-[#c9f31d] flex-1">
                                         <MdOutlineEmail className="text-2xl" />
@@ -57,7 +57,7 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div>
-                        <img className="max-w-[450px] h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
+                        <img className=" max-w-[380px]  md:max-w-[450px] mx-auto h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
                     </div>
                 </div>
             </div>
