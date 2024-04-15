629624a4b81b4db8fe5e7ad39a295af4b160da1a
tonoy3125 <shaifshajedt@gmail.com>
2024-04-15 22:17:39 +0600

    some chnages for mobile responsive

--- src/Components/About Me/AboutMe.jsx
+++ src/Components/About Me/AboutMe.jsx
@@ -8,12 +8,12 @@ const AboutMe = () => {
         <div className="bg-[#070707] pt-28 pb-28">
             <div className="max-w-7xl mx-auto">
                 <div className="flex flex-col lg:flex-row gap-20">
-                    <div className="semi-sm:mx-5 md:mx-10 lg:mx-0">
+                    <div className="sm:mx-3 semi-sm:mx-5 md:mx-10 lg:mx-0">
                         <h5 className="text-base font-medium text-white/50  mb-5 font-poppins">About Me</h5>
-                        <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium   font-poppins"><span className="text-[#fff]">Professional</span> <span className="text-[#c9f31d]">Problem Solutions</span>
+                        <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium   font-poppins"><span className="text-[#fff]">Professional</span> <span className="text-[#c9f31d]">Problem Solutions </span>
                             <span className="text-[#fff]">For Digital Products</span></h1>
                         <p className="text-base text-[#b1b1b1] font-poppins mt-7">At vero eos et accusamus etodio dignissimos ducimus praesentium voluptatum corrupti quos dolores quas molestias excepturi sint occaecati cupiditate provident qui officia deserunt mollitia animi, id est laborum et dolorum</p>
-                        <div className="flex flex-col md:flex-row md:items-center semi-sm:gap-5 md:gap-28 lg:gap-40 semi-sm:mt-5 md:mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center sm:gap-3 semi-sm:gap-5 md:gap-28 lg:gap-40 sm:mt-3 semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Branding & Design</span>
@@ -23,7 +23,7 @@ const AboutMe = () => {
                                 <span className="text-white text-xl font-lora font-semibold">Digital Marketing</span>
                             </div>
                         </div>
-                        <div className="flex flex-col md:flex-row md:items-center semi-sm:gap-5 md:gap-[115px] lg:gap-[164px] semi-sm:mt-5 md:mt-7">
+                        <div className="flex flex-col md:flex-row md:items-center sm:gap-3 semi-sm:gap-5 md:gap-[115px] lg:gap-[164px] sm:mt-3 semi-sm:mt-5 md:mt-7">
                             <div className="flex items-center gap-3">
                                 <IoMdCheckmark className="text-2xl text-[#c9f31d]" />
                                 <span className="text-white text-xl font-lora font-semibold">Web Development</span>
@@ -57,7 +57,7 @@ const AboutMe = () => {
                         </div>
                     </div>
                     <div>
-                        <img className=" max-w-[380px]  md:max-w-[450px] mx-auto h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
+                        <img className=" sm:max-w-[350px] semi-sm:max-w-[380px]  md:max-w-[450px] mx-auto md:h-[450px]" src="https://i.ibb.co/kHpnxzL/Gray-and-Orange-Simple-Linked-In-Profile-Picture.png" alt="" />
                     </div>
                 </div>
             </div>
