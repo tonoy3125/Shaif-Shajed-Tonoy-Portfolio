4c4c7d7ea4c2a8faa332cc1707ead079b751e382
tonoy3125 <shaifshajedt@gmail.com>
2024-04-15 13:09:06 +0600

    faq 2 question with ans added

--- src/Pages/Faq Accordion/FaqAccordionMain.jsx
+++ src/Pages/Faq Accordion/FaqAccordionMain.jsx
@@ -18,19 +18,25 @@ const FaqAccordionMain = () => {
                 <div className='flex-1 font-lora'>
                     <div className="accordion">
                         <div className={`accordion-item ${activeIndex === 1 ? 'active' : ''}`} onClick={() => toggleAccordion(1)}>
-                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>Click to open this one and close others</span></div>
+                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>What is your professional background and experience?</span></div>
                             <div className="accordion-content">
-                                <p className='text-white'>hello</p>
+                                <p className=''>I have a Master's degree in Department Of Economics from National University Of Bangladesh, and 2 years of learning experience in web developement field.Throughout my career, I've actively contributed to a variety of web development projects, refining my skills in HTML, CSS, JavaScript, and other relevant technologies. My experience includes designing responsive and visually appealing websites, optimizing performance, and ensuring cross-browser compatibility</p>
                             </div>
                         </div>
                         <div className={`accordion-item ${activeIndex === 2 ? 'active' : ''}`} onClick={() => toggleAccordion(2)}>
-                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>Click to open this one and close others</span></div>
+                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>What sets you apart from other candidates?</span></div>
+                            <div className="accordion-content">
+                                <p>What sets me apart is my unique combination of technical expertise, creativity, and strong communication skills.I am always eager to learn and adapt to new challenges.</p>
+                            </div>
+                        </div>
+                        <div className={`accordion-item ${activeIndex === 3 ? 'active' : ''}`} onClick={() => toggleAccordion(3)}>
+                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>Can you provide examples of your previous work?</span></div>
                             <div className="accordion-content">
                                 <p>hello</p>
                             </div>
                         </div>
                         <div className={`accordion-item ${activeIndex === 3 ? 'active' : ''}`} onClick={() => toggleAccordion(3)}>
-                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>Click to open this one and close others</span></div>
+                            <div className="accordion-title "><span className='accordian-inner'>{activeIndex === 1 ? <MdKeyboardArrowDown className='text-black text-xl' /> : <MdOutlineKeyboardArrowRight className='text-black text-xl' />}</span> <span>How do you handle tight deadlines or high-pressure situations?</span></div>
                             <div className="accordion-content">
                                 <p>hello</p>
                             </div>
