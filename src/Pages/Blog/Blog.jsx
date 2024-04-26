0c700e50e62e8f16ce3b9113372ce0d432ac1481
tonoy3125 <shaifshajedt@gmail.com>
2024-04-26 15:51:10 +0600

    blog section animation added by framer motion

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -2,16 +2,70 @@ import { MdOutlineArrowOutward } from "react-icons/md";
 import { FaCalendarAlt } from "react-icons/fa";
 import './Blog.css'
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { motion } from 'framer-motion';
 
 
 const Blog = () => {
+
+    const blogRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                }
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                // threshold: 0.5, // Trigger when 50% of the element is visible
+            }
+        );
+
+        if (blogRef.current) {
+            observer.observe(blogRef.current);
+        }
+
+        // Cleanup
+        return () => {
+            if (blogRef.current) {
+                observer.unobserve(blogRef.current);
+            }
+        };
+    }, []);
+
+
+
+
     return (
-        <div className="pb-28 pt-32 bg-[#070707]">
+        <div ref={blogRef} className="pb-28 pt-32 bg-[#070707] overflow-hidden">
             <div className="max-w-7xl mx-auto">
-                <h5 className="text-base font-medium text-white/50 text-center mb-5 font-poppins">New & Blog</h5>
-                <h1 className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium text-center px-2 font-poppins mb-20"><span className="text-[#fff]">Latest News & </span> <span className="text-[#c9f31d]">Blog</span></h1>
+                <motion.h5
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                    transition={{ duration: 2 }}
+                    className="text-base font-medium text-white/50 text-center mb-5 font-poppins"
+                >
+                    New & Blog
+                </motion.h5>
+                <motion.h1
+                    initial={{ y: -50, opacity: 0 }}
+                    animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                    transition={{ duration: 2, delay: 0.2 }}
+                    className="text-2xl sm:text-3xl semi-sm:text-4xl md:text-5xl font-medium text-center px-2 font-poppins mb-20"
+                >
+                    <span className="text-[#fff]">Latest News & </span> <span className="text-[#c9f31d]">Blog</span>
+                </motion.h1>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-7">
-                    <div className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer">
+                    <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                        transition={{ duration: 2, delay: 0.4 }}
+                        className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer"
+                    >
                         <div className="">
                             <img className="md:max-w-60 md:h-56 rounded-[10px] object-cover" src="https://i.ibb.co/XypBGfh/blog-09-min-768x417.jpg" alt="" />
                         </div>
@@ -27,8 +81,13 @@ const Blog = () => {
                             </Link>
                         </div>
 
-                    </div>
-                    <div className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer">
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                        transition={{ duration: 2, delay: 0.6 }}
+                        className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer"
+                    >
                         <div className="">
                             <img className="md:max-w-60 md:h-56 rounded-[10px] object-cover" src="https://i.ibb.co/R3CjrSj/blog-02-min-768x417.jpg" alt="" />
                         </div>
@@ -44,8 +103,13 @@ const Blog = () => {
                             </Link>
                         </div>
 
-                    </div>
-                    <div className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer">
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                        transition={{ duration: 2, delay: 0.8 }}
+                        className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer"
+                    >
                         <div className="">
                             <img className="md:max-w-60 md:h-56 rounded-[10px] object-cover" src="https://i.ibb.co/rcBrmXC/blog-04-min-768x417.jpg" alt="" />
                         </div>
@@ -61,8 +125,13 @@ const Blog = () => {
                             </Link>
                         </div>
 
-                    </div>
-                    <div className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer">
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 50, opacity: 0 }}
+                        animate={isVisible ? { y: 0, opacity: 1 } : {}}
+                        transition={{ duration: 2, delay: 1 }}
+                        className="border border-[#353535] hover:border-[#c9f31d] bg-[#292828] lg:w-[630px] rounded-[15px] flex flex-col md:flex-row items-center gap-10 xs:pl-2 xs:pr-2 xs:pt-2 semi-sm:pl-3 semi-sm:pr-3 semi-sm:pt-3 md:pl-2 md:pt-2 md:pb-2 md:pr-10 card-container xs:mx-3 sm:mx-4 semi-sm:mx-5 md:mx-4 lg:mx-0 cursor-pointer"
+                    >
                         <div className="">
                             <img className="md:max-w-60 md:h-56 rounded-[10px] object-cover" src="https://i.ibb.co/gF38mtp/blog-05-min-768x417.jpg" alt="" />
                         </div>
@@ -78,7 +147,7 @@ const Blog = () => {
                             </Link>
                         </div>
 
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
